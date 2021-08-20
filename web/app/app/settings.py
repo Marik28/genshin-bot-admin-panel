2af0f2a3ed9f8ccd39c414c4674bf096bf0dcdcb
mark <markel20002000@gmail.com>
2021-08-20 21:28:50 +0600

    напоминание

--- web/app/app/settings.py
+++ web/app/app/settings.py
@@ -23,6 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# todo переделать
 DEBUG = os.getenv("DJANGO_DEBUG", False)
 
 ALLOWED_HOSTS = ['127.0.0.1', '135.125.191.18']
